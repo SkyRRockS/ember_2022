9c461077030e4fb960cd9c0b497801a9d0e7e349
Youn Hollande <94164185+SkyRRockS@users.noreply.github.com>
2022-01-28 11:37:19 +0100

    Background Color
    The background can have 3 colors :
    - red if you wrote 80 characters
    - orange if you wrote 50 characters
    - blue in the others cases

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -1,12 +1,31 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import {action} from '@ember/object';
 
 export default class Ex1Controller extends Controller {
   @tracked content = 'Texte par defaut';
+  @tracked info = '';
   max = 100;
 
   get size(){
-    return this.content.length;
+    return this.max - this.content.length;
+  }
+
+  get style(){
+    if(this.size < 20){
+      return 'danger';
+    }
+    if(this.size < 50){
+      return 'warning';
+    }
+      return 'info';
+  }
+
+
+
+  @action
+  test(){
+    this.content = '';
   }
 
 }
