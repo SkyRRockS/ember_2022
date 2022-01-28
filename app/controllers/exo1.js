3861f59eac217540edd2a50b2af1c602d944f610
Youn Hollande <94164185+SkyRRockS@users.noreply.github.com>
2022-01-28 10:06:13 +0100

    Controlleur ajout
    controlleur

---
+++ app/controllers/exo1.js
@@ -0,0 +1,3 @@
+import Controller from '@ember/controller';
+
+export default class Exo1Controller extends Controller {}
---
+++ tests/unit/controllers/exo1-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Controller | exo1', function (hooks) {
+  setupTest(hooks);
+
+  // TODO: Replace this with your real tests.
+  test('it exists', function (assert) {
+    let controller = this.owner.lookup('controller:exo1');
+    assert.ok(controller);
+  });
+});
